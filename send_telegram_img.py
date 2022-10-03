ba4d4bc8539c305c30aa8492bb3e167141fa4a69
Swatlprus <swatlprus@gmail.com>
2022-10-03 09:52:15 +0500

    Add extra check variable sleep_time

--- send_telegram_img.py
+++ send_telegram_img.py
@@ -39,9 +39,12 @@ def main():
     parser.add_argument('--path_to_images', default='images')
     args = parser.parse_args()
     telegram_chat_id = env("TELEGRAM_CHAT_ID")
-    if env("SLEEP_TIME"):
-        sleep_time = env("SLEEP_TIME")
-    else:
+    try:
+        if env("SLEEP_TIME"):
+            sleep_time = env("SLEEP_TIME")
+        else:
+            sleep_time = 14400
+    except:
         sleep_time = 14400
     post_img_telegram(sleep_time, telegram_token, telegram_chat_id, args.path_to_images)
 
